1fede64d4a164d0bd16a345dcc7c54b8acb545ca
adal347 <adalaidc@acm.org>
2017-04-18 17:54:20 -0500

    initial configuration

---
+++ perceptron.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <sstream>
+
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  
+  return 0;
+}
